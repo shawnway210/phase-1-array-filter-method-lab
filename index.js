9ded234fee244959023018d48e49491f5a599aae
shawnconway <shawnway210@gmail.com>
2023-08-25 12:54:56 -0400

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+ function findMatching(drivers, inputName){
+    const result = drivers.filter((driver) => driver.toLowerCase() === inputName.toLowerCase())
+   console.log('Input Name:', inputName)
+   console.log('Filtered Drivers:', result)
+   return result
+}   
+   findMatching("Bobby")
+   findMatching("Sammy")
+   const drivers = [ 
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh' },
+    {
+        name: 'Sammy',
+        hometown: 'New York'},
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'},
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'},
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'}
+  ]
+function fuzzyMatch(drivers, inputName){ 
+    const result = drivers.filter((driver)=> driver.toLowerCase().startsWith(inputName.toLowerCase()))
+    return result 
+}
+  fuzzyMatch( drivers, "S")
+  
+
+  function matchName(drivers, homeTown){
+    const result = drivers.filter((driver) => driver.name === homeTown)
+    return result
+}
+  matchName(drivers, "Bobby")
